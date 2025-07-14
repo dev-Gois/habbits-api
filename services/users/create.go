a9a92e4557c572adb7dc342e97b1ba770aa2f9e2
Matheus Gois <mattheusgoiis@gmail.com>
2025-07-13 22:46:13 -0300

    feat: translate messages

--- services/users/create.go
+++ services/users/create.go
@@ -11,16 +11,16 @@ import (
 func Create(user models.User) (models.User, string, error) {
 	var existing models.User
 	if err := config.DB.Where("email = ?", user.Email).First(&existing).Error; err == nil {
-		return models.User{}, "", fmt.Errorf("email already in use")
+		return models.User{}, "", fmt.Errorf("Email já está sendo utilizado!")
 	}
 
 	if err := user.Create(); err != nil {
-		return models.User{}, "", fmt.Errorf("failed to create user: %w", err)
+		return models.User{}, "", fmt.Errorf("Erro ao criar usuário: %w", err)
 	}
 
 	token, err := jwt.Encode(user.ID)
 	if err != nil {
-		return models.User{}, "", fmt.Errorf("failed to generate token: %w", err)
+		return models.User{}, "", fmt.Errorf("Erro ao gerar token: %w", err)
 	}
 
 	return user, token, nil
